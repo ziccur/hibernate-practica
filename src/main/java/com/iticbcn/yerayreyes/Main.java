91fc4860a04db8f935c483623bed272e3d89c3c4
Yeray <2023_yeray.reyes@iticbcn.cat>
2025-02-10 20:19:40 +0100

    primer deploy exitos

--- src/main/java/com/iticbcn/yerayreyes/Main.java
+++ src/main/java/com/iticbcn/yerayreyes/Main.java
@@ -1,7 +1,22 @@
 package com.iticbcn.yerayreyes;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main( String[] args )
+    {
+        SessionFactory sesion = HibernateUtil.getSessionFactory();
+
+        Session session = sesion.openSession();
+
+        session.beginTransaction();
+
+        System.out.println("Hola desde Hibernate");
+
+        session.getTransaction().commit();
+
+        session.close();
+
     }
 }
